a96f8eec5fb5e74ed9d86b0cad07be8ae9013eac
demoB33 <vasilenko-s@list.ru>
2023-09-10 01:17:26 +0700

    Changed void methods to String (Shelter.interface and SendAnimalData.interface)

--- src/main/java/tg/kindhands_bot/kindhands/send_data/SendCatData.java
+++ src/main/java/tg/kindhands_bot/kindhands/send_data/SendCatData.java
@@ -3,36 +3,36 @@ package tg.kindhands_bot.kindhands.send_data;
 public class SendCatData implements SendAnimalData{
     @Override
     public String PrintTheAnimalIntroductionRules() {
-        System.out.println("правила знакомства с кошкой до того, как забрать из приюта.");
+        return "правила знакомства с кошкой до того, как забрать из приюта.";
     }
 
     @Override
     public String printListOfDocuments() {
-        System.out.println("список документов, необходимых для того, чтобы взять кошку из приюта.");
+        return "список документов, необходимых для того, чтобы взять кошку из приюта.";
     }
 
     @Override
     public String printRecommendationsForTransporting() {
-        System.out.println("список рекомендаций по транспортировке кошки.");
+        return "список рекомендаций по транспортировке кошки.";
     }
 
     @Override
     public String printRecommendationsForHouseSmallAnimal() {
-        System.out.println("список рекомендаций по обустройству дома для котенка");
+        return "список рекомендаций по обустройству дома для котенка";
     }
 
     @Override
     public String printRecommendationsForHouseAdultAnimal() {
-        System.out.println("список рекомендаций по обустройству дома для взрослой кошки");
+        return "список рекомендаций по обустройству дома для взрослой кошки";
     }
 
     @Override
     public String printRecommendationsForHouseDisabledAnimal() {
-        System.out.println("список рекомендаций по обустройству дома для кошки с ограниченными возможностями");
+        return "список рекомендаций по обустройству дома для кошки с ограниченными возможностями";
     }
 
     @Override
     public String printRejectionReason() {
-        System.out.println("Выдать список причин, почему могут отказать и не дать забрать кошку из приюта.");
+        return "Выдать список причин, почему могут отказать и не дать забрать кошку из приюта.";
     }
 }
