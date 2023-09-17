665da222cf5e904a26912309a49f98807dccdcaa
PC <ya.brodskii@yandex.ru>
2023-09-17 22:06:24 +0300

    Начал реализацию тестов

--- src/main/java/tg/kindhands_bot/kindhands/entities/User.java
+++ src/main/java/tg/kindhands_bot/kindhands/entities/User.java
@@ -66,13 +66,6 @@ public class User {
         this.botState = botState;
     }
 
-    public String getDenialReason() {
-        return denialReason;
-    }
-
-    public void setDenialReason(String denialReason) {
-        this.denialReason = denialReason;
-    }
 
     @Override
     public boolean equals(Object o) {
